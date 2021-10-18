5d70abb4f00e86914baa93ee0edb4988a1975b6a
GodzFeivur <godzfeivvur@gmail.com>
2021-10-18 21:02:51 +0100

    update

--- Source/Component/script.js
+++ Source/Component/script.js
@@ -1,14 +1,6 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+let email = document.getElementById('email');
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
+function validates() {
+    console.log(email.value)
+    alert('Thanks! you will receive a feedback from us')
 }
