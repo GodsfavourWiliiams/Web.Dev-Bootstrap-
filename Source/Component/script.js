31676e9a62bc4aa14dfcba15c67299b49cdacbe0
GodzFeivur <godzfeivvur@gmail.com>
2021-10-26 22:39:10 +0100

    carousel

--- Source/Component/script.js
+++ Source/Component/script.js
@@ -1,67 +1,14 @@
 const gap = 16;
 const navMenu = document.querySelector("#navMenu");
-const Slider = document.querySelector(".slider");
 const Btns = document.querySelectorAll(".btn");
-const slideImgs = document.querySelectorAll(".img");
 
 
 navMenu.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
 
-
-
-
-let index = 1;
-let imgWidth = slideImgs[index].clientWidth;
-
-window.addEventListener('resize', () => {
-    imgWidth = slideImgs[index].clientWidth;
-})
-
-function slide() {
-    Slider.style.transition = 'transform .5s ease-in-out';
-    Slider.style.transform = 'translateX(' + (-imgWidth * index) + 'px)';
-}
-
-function btnClick() {
-    if (this.id === 'prev') {
-        index--;
-    } else {
-        index++;
-    }
-    this.disabled = true
-    slide();
-}
-
-Slider.addEventListener('transitionend', () => {
-
-    if (slideImgs[index].id === 'first') {
-        Slider.style.transition = 'none';
-        index = slideImgs.length - 1;
-        Slider.style.transform = 'translateX(' + (-imgWidth * index) + 'px)';
-    } else if (slideImgs[index].id === 'last') {
-        Slider.style.transition = 'none';
-        index = 1;
-        Slider.style.transform = 'translateX(' + (-imgWidth * index) + 'px)';
-    }
-    Btns[0].disabled = false
-    Btns[1].disabled = false
-})
-
-Btns.forEach(btn => btn.addEventListener('click', btnClick));
-
-
-var width = carousel.offsetWidth;
-window.addEventListener("resize", e => (width = carousel.offsetWidth));
-
-function clicked() {
-    // var randoms = randomArray[]
-    contentOne.innerHTML = randomArray[0]
-    contentTwo.innerHTML = randomArray[2]
-}
-
 /*===== MENU SHOW =====*/
+
 const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId)
