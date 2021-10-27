b77fa7557536f5a1564cd1ff8790118386aa482e
GodzFeivur <godzfeivvur@gmail.com>
2021-10-27 14:16:22 +0100

    update

--- Source/Component/script.js
+++ Source/Component/script.js
@@ -1,12 +1,35 @@
-const gap = 16;
 const navMenu = document.querySelector("#navMenu");
 const Btns = document.querySelectorAll(".btn");
+const my = document.getElementById("modal");
+const modal = document.getElementById("myModal");
+const body = document.getElementById("body");
+const span = document.getElementsByClassName("blah")[0];
+// When the user clicks the button, open the modal 
+my.addEventListener("click", () => {
+    modal.style.display = "block";
+    body.style.overflow = "hidden"
 
+});
+// When the user clicks on <span> (x), close the modal
+
+span.addEventListener("click", () => {
+    modal.style.display = "none";
+    body.style.overflow = "visible"
+});
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        // body.style.overflow = "visible"
+
+    }
+};
 
 navMenu.addEventListener("click", () => {
     navMenu.classList.toggle("active");
 });
-
 /*===== MENU SHOW =====*/
 
 const showMenu = (toggleId, navId) => {
@@ -16,6 +39,8 @@ const showMenu = (toggleId, navId) => {
     if (toggle && nav) {
         toggle.addEventListener('click', () => {
             nav.classList.toggle('show')
+
+
         })
     }
 }
@@ -32,5 +57,8 @@ function linkAction() {
     /*Remove menu mobile*/
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show')
+
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// Get the modal
