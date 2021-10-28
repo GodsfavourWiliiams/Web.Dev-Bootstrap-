d7531ed936f7b2f6e345c9e7a9c465f779a1ecbc
GodzFeivur <godzfeivvur@gmail.com>
2021-10-28 20:36:19 +0100

    form validatiom

--- Source/Component/script.js
+++ Source/Component/script.js
@@ -1,14 +1,100 @@
 const navMenu = document.querySelector("#navMenu");
 const Btns = document.querySelectorAll(".btn");
-const my = document.getElementById("modal");
+const myUp = document.getElementById("modal");
+const myIn = document.getElementById("modalsignin");
 const modal = document.getElementById("myModal");
 const body = document.getElementById("body");
+const title = document.getElementById("form-title");
 const span = document.getElementsByClassName("blah")[0];
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    // trim to remove the whitespaces
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if (usernameValue === '') {
+        setErrorFor(username, 'Username cannot be blank');
+    } else {
+        setSuccessFor(username);
+    }
+
+    if (emailValue === '') {
+        setErrorFor(email, 'Email cannot be blank');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Not a valid email');
+    } else {
+        setSuccessFor(email);
+    }
+
+    if (passwordValue === '') {
+        setErrorFor(password, 'Password cannot be blank');
+    } else {
+        setSuccessFor(password);
+    }
+
+    if (password2Value === '') {
+        setErrorFor(password2, 'Password2 cannot be blank');
+    } else if (passwordValue !== password2Value) {
+        setErrorFor(password2, 'Passwords does not match');
+    } else {
+        setSuccessFor(password2);
+    }
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+function isEmail(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+    social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+    social_panel_container.classList.remove('visible')
+});
 // When the user clicks the button, open the modal 
-my.addEventListener("click", () => {
+myUp.addEventListener("click", () => {
     modal.style.display = "block";
     body.style.overflow = "hidden"
-
+    title.textContent = "Sign up";
+    // Btns.value = "Sign up";
+});
+myIn.addEventListener("click", () => {
+    modal.style.display = "block";
+    body.style.overflow = "hidden";
+    title.textContent = "Sign in";
+    // Btns.textContent = "Login";
 });
 // When the user clicks on <span> (x), close the modal
 
@@ -16,13 +102,21 @@ span.addEventListener("click", () => {
     modal.style.display = "none";
     body.style.overflow = "visible"
 });
+// showError when inputs is empty or zero
+
+//Submit button function
+function submit() {
+    modal.style.display = "none";
+    // check: if all input is empty show alert
+
+}
 
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
-        // body.style.overflow = "visible"
+        body.style.overflow = "visible"
 
     }
 };
@@ -61,4 +155,34 @@ function linkAction() {
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
+// function validateForm() {
+//     var x = document.forms.value;
+//     if (x == null || x == "") {
+//         const enu = document.getElementById('enu');
+//         enu.classList.add('shows');
+//         // disapear the message after 3 sec
+//         setTimeout(function disApear() {
+//             enu.classList.remove("shows");
+//         }, 2000);
+//         const enuu = document.getElementById('enuu');
+//         enuu.classList.add('shows');
+//         setTimeout(function disApear() {
+//             enuu.classList.remove("shows");
+//         }, 2000);
+//         // disapear the message after 3 sec
+//         const enuuu = document.getElementById('enuuu');
+//         enuuu.classList.add('shows');
+//         setTimeout(function disApear() {
+//             enuuu.classList.remove("shows");
+//         }, 2000);
+
+//         const enuuuu = document.getElementById('enuuuu');
+//         enuuuu.classList.add('shows');
+//         // disapear the message after 3 sec
+//         setTimeout(function disApear() {
+//             enuuuu.classList.remove("shows");
+//         }, 2000);
+//         return false;
+//     } else {}
+// };
 // Get the modal
